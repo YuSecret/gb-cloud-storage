2b4e8885fa21524d381947b51bbf0da70ce804f2
YuSecret <4sovremennik@gmail.com>
2020-05-28 17:45:44 +0300

    1

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -4,11 +4,21 @@ import java.util.Arrays;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String filePath = "client\\src\\main\\resources\\ava.png";
+        String fileName = "ava.png";
 
-        File file = new File(filePath);
+        String currentPath = Client.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        int ind = currentPath.indexOf("client")+"client".length();
+        currentPath = currentPath.substring(0,ind);
+        System.out.println("currentPath: "+currentPath);
+        File dir = new File(currentPath);
+        File[] arrFiles = dir.listFiles();
+        for (int j=0 ; j<arrFiles.length; j++) {
+            System.out.println(arrFiles[j]);
+        }
+
+        File file = new File(currentPath+"\\"+fileName);
         if (file.exists()) {
-            System.out.println("File: "+filePath+" exist! Size: "+file.length());
+            System.out.println("File: "+fileName+" exist! Size: "+file.length());
         }
         else {
             throw new FileNotFoundException("Такова нетуЭ");
